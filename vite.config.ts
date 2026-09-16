09465a9b8311d73444580b8779fdc8371d34d6eb
Esteban2700 <tusa_1000@hotmail.com>
2026-09-16 11:32:25 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/app2/',
+  base: '/app2/',  // <--- ESTO ES LO QUE FALTA O ESTÁ MAL
   plugins: [react(), tailwindcss()],
 })
