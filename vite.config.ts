0adc7a685b933443bbd2733a755171a5efdda13d
Esteban2700 <tusa_1000@hotmail.com>
2026-09-16 09:01:27 +0000

    Configurar scripts de despliegue y base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/app2/',
   plugins: [react(), tailwindcss()],
   base: '/app2/', // Importante: debe coincidir con el nombre del repositorio
 })
